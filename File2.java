380dab91976734b918c72ffacbcaf1af4377131a
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 18:32:32 -0300

    Update File2.java

--- File2.java
+++ File2.java
@@ -1,11 +1,3 @@
-/*
-Atividade 2
-
-Vamos praticar!!
-▪ Faça uma função que recebe um path de um arquivo de texto e conta quantas linhas tem
-no arquivo
-▪ Se o arquivo não existe, isso deve ser informado na tela
- */
 package file2;
 
 import java.io.*;
